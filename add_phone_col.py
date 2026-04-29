48904286e736a143fe3c8abce19fa501c0624ecc
pguitemberg <pguitemberg@gmail.com>
2026-04-29 03:23:42 -0400

    Dynamic client_id from phone number

---
+++ add_phone_col.py
@@ -0,0 +1,8 @@
+﻿from dotenv import load_dotenv
+load_dotenv('D:/agents/projects/AAA-HVAC-AI/.env')
+from db.postgres import get_conn
+with get_conn() as conn:
+    with conn.cursor() as cur:
+        cur.execute('ALTER TABLE clients ADD COLUMN IF NOT EXISTS phone_number VARCHAR(50)')
+    conn.commit()
+print('Done!')
---
+++ update_phone.py
@@ -0,0 +1,8 @@
+﻿from dotenv import load_dotenv
+load_dotenv('D:/agents/projects/AAA-HVAC-AI/.env')
+from db.postgres import get_conn
+with get_conn() as conn:
+    with conn.cursor() as cur:
+        cur.execute('UPDATE clients SET phone_number = %s WHERE username = %s', ('+16312065719', 'abc-cooling'))
+    conn.commit()
+print('Done!')
