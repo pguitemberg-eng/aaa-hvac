53ec092183897f88ff37f3b0379680b7f05421fa
pguitemberg <pguitemberg@gmail.com>
2026-04-30 02:33:41 -0400

    redesign: professional client dashboard

---
+++ final_check.py
@@ -0,0 +1,12 @@
+﻿import psycopg2, hashlib
+from dotenv import load_dotenv
+import os
+load_dotenv()
+conn = psycopg2.connect(os.getenv("DATABASE_URL"))
+with conn.cursor() as cur:
+    hashed = hashlib.sha256('midvio123'.encode()).hexdigest()
+    cur.execute("SELECT id, company_name, username FROM clients WHERE username='abc-cooling' AND password_hash=%s", (hashed,))
+    print("LOGIN RESULT:", cur.fetchone())
+    cur.execute("SELECT id, client_id, name FROM leads")
+    print("ALL LEADS:", cur.fetchall())
+conn.close()
---
+++ show_all.py
@@ -0,0 +1,9 @@
+﻿import psycopg2
+from dotenv import load_dotenv
+import os
+load_dotenv()
+conn = psycopg2.connect(os.getenv("DATABASE_URL"))
+with conn.cursor() as cur:
+    cur.execute("SELECT * FROM clients")
+    print("CLIENTS:", cur.fetchall())
+conn.close()
